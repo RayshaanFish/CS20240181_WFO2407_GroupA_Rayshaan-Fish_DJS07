b3e378b99aabd1e2f66ee9a60383b58f1af54e9d
Rayshaan Fish <Rayshaan94@gmail.com>
2024-11-15 13:20:28 +0200

    added meme component with basic styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
-    <header>
-      <h2>Header component</h2>
+    <header className="header">
+      <img src="src/images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
